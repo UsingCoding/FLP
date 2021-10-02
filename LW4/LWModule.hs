ee4a660f08bd9fc43a9246a55eb9de2d3a7979a5
UsingCoding <extendedmoment@outlook.com>
2021-10-02 17:50:58 +0000

    Добавил ещё функции по заданию

--- LW4/LWModule.hs
+++ LW4/LWModule.hs
@@ -1,6 +1,21 @@
-module LWModule (intersperse) where
+module LWModule (intersperse, filter, toUpper) where
 
+import Data.Set as Set hiding (filter)
+import Data.Char as Char hiding (toUpper)
+import Data.List as List hiding (intersperse, filter)
+
+import Prelude hiding (filter)
+
+-- List
 intersperse :: a -> [a] -> [a]
 intersperse _ [] = []
 intersperse _ [x] = [x]
 intersperse x list = head list : x : intersperse x (tail list)
+
+-- Set
+filter :: (Ord a) => (a -> Bool) -> Set a -> Set a
+filter predicate set = Set.fromList( [x | x <- Set.toList set, predicate x] )
+
+
+toUpper :: Char -> Char
+toUpper ch = if ch `elem` ['a'..'z'] then chr ( ord ch - 32) else ch
--- LW4/main.hs
+++ LW4/main.hs
@@ -1,12 +1,27 @@
 --ghc 8.0.2
 import LWModule
 
+import Data.Set as Set
+
 -- Data.List - intersperse
 -- Data.Char - toUpper
 -- Data.Map - deleteAt
--- Data.Map - filter
+-- Data.Map - mapMaybe
 -- Data.Set - filter
 
 main :: IO ()
 main = do
+    -- List
     print( LWModule.intersperse '.' "STALKER" )
+
+    -- Set
+    let specialSet = Set.fromList [1, 2, 3]
+
+    print( LWModule.filter (== 3) specialSet )
+
+    -- Char
+    print( LWModule.toUpper 'a' )
+
+    print( LWModule.toUpper 'W' )
+
+    print( LWModule.toUpper '#' )
