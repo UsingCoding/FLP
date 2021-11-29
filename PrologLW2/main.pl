26c2efe522f30e3de1d0eeaeb112c260b0ba37b2
UsingCoding <extendedmoment@outlook.com>
2021-11-29 19:18:33 +0300

    Добавил правки по лабораторной

--- PrologLW2/main.pl
+++ PrologLW2/main.pl
@@ -11,13 +11,14 @@ seg(9, point(12, 2), point(12, 12)).
 
 horiz(N):-seg(N, point(_,Y), point(_,Y)).
 
-vertical(N):-seg(N, point(X,_), point(X,)).
+vertical(N):-seg(N, point(X,_), point(X,_)).
 
 cross(N, M, point(X, Y), L1, L2):-seg(M, point(X1, Y), point(X2, Y)), seg(N, point(X, Y3), point(X, Y4)),
 Y4 > Y, Y3 < Y, X > X1, X < X2, L1 is Y4 - Y3, L2 is X2 - X1.
 
-perimetr(N1, N2, N3, N4, P, S):-cross(N1, N2, point(X1, Y1), _, _), cross(N3, N2, point(X2, Y2), _, _),
-cross(N3, N4, point(X3, Y3), _, _), cross(N1, N4, point(X4, Y4), _, _), Y1 = Y2, Y3 = Y4, X1 = X4, X2 = X3,
-X1 < X2, Y2 > Y3, X3 > X4, Y1 > Y4,
-N1 \= N2, N2 \= N3, N3 \= N4, N1 \= N4, N1 \= N3, N2 \= N4,
-P is (X2 - X1) + (Y2 - Y3) + (X3 - X4) + (Y1 - Y4), S is (X2 - X1) * (Y2 - Y3), P > 0, S > 0.
+perimetr(N1, N2, N3, N4, P, S):-cross(N1, N2, point(X1, Y1), Len1, Len2),
+cross(N3, N2, point(X2, Y1), Len3, Len4),
+cross(N3, N4, point(X2, Y2), Len5, Len6),
+cross(N1, N4, point(X1, Y2), Len7, Len8),
+P is (X2 - X1) * 2 + (Y1 - Y2)  * 2,
+S is (X2 - X1) * (Y1 - Y2), P > 0, S > 0.
