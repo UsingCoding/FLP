4819c16bc85185f10a6fd8cd46bd1177c0500662
vadim.makerov <vadim.makerov@ispring.com>
2021-12-01 13:08:39 +0300

    Небольшие правки по лабораторной

--- PrologLW2/main.pl
+++ PrologLW2/main.pl
@@ -16,9 +16,9 @@ vertical(N):-seg(N, point(X,_), point(X,_)).
 cross(N, M, point(X, Y), L1, L2):-seg(M, point(X1, Y), point(X2, Y)), seg(N, point(X, Y3), point(X, Y4)),
 Y4 > Y, Y3 < Y, X > X1, X < X2, L1 is Y4 - Y3, L2 is X2 - X1.
 
-perimetr(N1, N2, N3, N4, P, S):-cross(N1, N2, point(X1, Y1), Len1, Len2),
-cross(N3, N2, point(X2, Y1), Len3, Len4),
-cross(N3, N4, point(X2, Y2), Len5, Len6),
-cross(N1, N4, point(X1, Y2), Len7, Len8),
+perimetr(N1, N2, N3, N4, P, S):-cross(N1, N2, point(X1, Y1), _, _),
+cross(N3, N2, point(X2, Y1), _, _),
+cross(N3, N4, point(X2, Y2), _, _),
+cross(N1, N4, point(X1, Y2), _, _),
 P is (X2 - X1) * 2 + (Y1 - Y2)  * 2,
 S is (X2 - X1) * (Y1 - Y2), P > 0, S > 0.
